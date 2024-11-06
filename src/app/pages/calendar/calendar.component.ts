73e03add97fe0a79fd4e9ba8518a13175a2438f3
mohammadmaleki465@gmail.com <mohammadmaleki465@gmail.com>
2024-11-06 16:43:39 +0330

    develop

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,11 +1,15 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { HeaderComponent } from '@components/header/header.component';
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [MatSidenavModule, HeaderComponent],
+  imports: [MatSidenavModule, MatButtonModule, NgFor],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss',
 })
-export class CalendarComponent {}
+export class CalendarComponent {
+  divs = Array(100).fill(0).map((_, index) => index + 1);
+}
